f622402fa673bd967892b53b052385f66b5f430f
2580talhahabib <tsab70809@gmail.com>
2025-04-08 12:24:39 +0500

    Design Ui App jsx

---
+++ frontant/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='bg-dark text-white'>
+<h4>React & Blog App</h4>
+    </div>
+    </>
+  )
+}
+
+export default App
